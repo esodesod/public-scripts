42c7925a47db277fbbe600ae42221cb88b90001e
Espen Ødegaard <esod@esod.no>
2017-11-20 13:52:01 +0100

    Added VMware-scripts

---
+++ powershell/vmware/00-remove-hyper-v-conflicts.ps1
@@ -0,0 +1,5 @@
+﻿#Check cmdlets in conflict (duplicates)
+gcm -type cmdlet,function,alias | group name | where { $_.count -gt 1 }
+
+#Remove Hyper-V module
+(Get-Module -Name Hyper-V) | Remove-Module -Force
