8328f6c792db72b51bf39dd15566acaad4075626
Espen Ødegaard <esodesod@users.noreply.github.com>
2018-01-24 10:11:04 +0100

    First commit

---
+++ powershell/vmware/network-nic-get-cdp-all-tengig.ps1
@@ -0,0 +1,2 @@
+﻿# Uses function Get-VMHostNetworkAdapterCDP
+get-cluster "clustername" | get-vmhost | Get-VMHostNetworkAdapterCDP | where PortId -Like 'TenGig*' | sort switch | ft switch,portid,nic,vmhost -AutoSize
