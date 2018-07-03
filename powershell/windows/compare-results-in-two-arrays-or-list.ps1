7ffe490008b96ede126f758eb463a87d8a079935
Espen Ødegaard <esodesod@users.noreply.github.com>
2018-07-03 10:27:01 +0200

    Added compare of reparsepoint files

--- powershell/windows/compare-results-in-two-arrays-or-list.ps1
+++ powershell/windows/compare-results-in-two-arrays-or-list.ps1
@@ -1,8 +1,18 @@
 ﻿# Compare results of two arrays
-$src = gci "C:\temp\powershell\src" -recurse | select -ExpandProperty Name
-$dst = gci "C:\temp\powershell\dst" -recurse | select -ExpandProperty Name
+$src = gci -file "C:\temp\powershell\src" -recurse | select -ExpandProperty Name
+$dst = gci -file "C:\temp\powershell\dst" -recurse | select -ExpandProperty Name
 
 $src | ?{$dst -notcontains $_}
+
+
+# A little more complex - compare results of two arrays - find reparsepoint files (ignore folders)
+#$src = gci -file "C:\temp\powershell\src" -recurse | where {$_.attributes -match 'ReparsePoint' -and ! $_.PSIsContainer } | select -ExpandProperty name
+#$dst = gci -file "C:\temp\powershell\dst" -recurse |  where {! $_.PSIsContainer } | select -ExpandProperty name
+#$src | ?{$dst -notcontains $_}
+# Measure results, and display count
+#$dst | measure | % {$_.count }
+#$src | measure | % {$_.count }
+
 
 # Same, but with Compare-Object
 # Compare-Object $src $dst | ?{$_.SideIndicator -eq '<='} | Select-Object -ExpandProperty InputObject
