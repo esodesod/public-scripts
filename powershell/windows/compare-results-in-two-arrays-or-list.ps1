4c5b0dcedecebb193b7f541370d8251b61f6e609
Espen Ødegaard <esodesod@users.noreply.github.com>
2018-07-03 10:03:17 +0200

    New compare-file

---
+++ powershell/windows/compare-results-in-two-arrays-or-list.ps1
@@ -0,0 +1,8 @@
+﻿# Compare results of two arrays
+$src = gci "C:\temp\powershell\src" -recurse | select -ExpandProperty Name
+$dst = gci "C:\temp\powershell\dst" -recurse | select -ExpandProperty Name
+
+$src | ?{$dst -notcontains $_}
+
+# Same, but with Compare-Object
+# Compare-Object $src $dst | ?{$_.SideIndicator -eq '<='} | Select-Object -ExpandProperty InputObject
