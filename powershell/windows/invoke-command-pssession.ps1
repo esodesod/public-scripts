c3ed96eb7f3a74ab69547ef2d6568054382319b4
Espen Ødegaard <esodesod@users.noreply.github.com>
2018-08-03 14:22:17 +0200

    update

--- powershell/windows/invoke-command-pssession.ps1
+++ powershell/windows/invoke-command-pssession.ps1
@@ -2,5 +2,11 @@
 $cred = Get-Credential
 
 # See https://docs.microsoft.com/en-us/powershell/scripting/core-powershell/running-remote-commands?view=powershell-6
+
+# One compute directly
+Invoke-Command -ComputerName computer1 {get-eventlog system | where-object {$_.eventid -eq 1074} | select -first 10} -Credential $cred | Out-GridView
+
+# Multiple computers at once
 $s = New-PSSession computer1,computer2,computer3 -Credential $cred
 Invoke-Command -Session $s {ls C:\win*}
+
