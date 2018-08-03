4b282e7ee8b581a5a19b5b6641fe2eb73a8665f8
Espen Ødegaard <esodesod@users.noreply.github.com>
2018-08-03 14:23:52 +0200

    update

--- powershell/windows/get-eventlog-where-object-6005-system-powered-on-boot-reboot-event-log-started.ps1
+++ powershell/windows/get-eventlog-where-object-6005-system-powered-on-boot-reboot-event-log-started.ps1
@@ -1,2 +1,7 @@
 # Search for event "6005", aka "The Event log service was started."
 get-eventlog system | where-object {$_.eventid -eq 6005} | select -first 10
+
+# Search for event "1074"
+get-eventlog system | where-object {$_.eventid -eq 1074} | select -first 10 | out-gridview
+
+# See https://docs.microsoft.com/en-us/powershell/scripting/core-powershell/running-remote-commands?view=powershell-6
