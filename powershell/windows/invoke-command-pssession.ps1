31c318d457610897fce4b9245430db7e9cab743b
Espen Ødegaard <esodesod@users.noreply.github.com>
2018-08-03 13:24:13 +0200

    new

---
+++ powershell/windows/invoke-command-pssession.ps1
@@ -0,0 +1,6 @@
+﻿# Ask & save credential to variable
+$cred = Get-Credential
+
+# See https://docs.microsoft.com/en-us/powershell/scripting/core-powershell/running-remote-commands?view=powershell-6
+$s = New-PSSession computer1,computer2,computer3 -Credential $cred
+Invoke-Command -Session $s {ls C:\win*}
