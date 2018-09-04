4fbc1e6d0c27e66817a9856f3227c5cf0f94474e
Espen Ødegaard <esodesod@users.noreply.github.com>
2018-09-04 09:48:31 +0200

    Small improvement

--- powershell/windows/compare-results-in-two-arrays-or-list.ps1
+++ powershell/windows/compare-results-in-two-arrays-or-list.ps1
@@ -20,7 +20,7 @@ $files_src.replace("$root_src","ROOT") | ?{$files_dst.replace("$root_dst","ROOT"
 $root_src = "C:\temp\powershell\src"
 $root_dst = "C:\temp\powershell\dst"
 $files_src = gci $root_src -recurse | where {$_.attributes -match 'ReparsePoint' -and ! $_.PSIsContainer } | select -ExpandProperty fullname
-$files_dst = gci $root_dst -recurse |  where {! $_.PSIsContainer } | select -ExpandProperty fullname
+$files_dst = gci $root_dst -recurse | where {! $_.PSIsContainer } | select -ExpandProperty fullname
 # Measure results, and display count
 $files_src | measure | % {$_.count }
 $files_dst | measure | % {$_.count }
@@ -29,3 +29,26 @@ $files_dst | measure | % {$_.count }
 # Compare results, but using Compare-Object (faster)
 Compare-Object -ReferenceObject $(($files_src).replace("$root_src","ROOT")) -DifferenceObject $(($files_dst).replace("$root_dst","ROOT")) | Where { $_.SideIndicator -eq '<=' }
 Compare-Object -ReferenceObject $(($files_src).replace("$root_src","ROOT")) -DifferenceObject $(($files_dst).replace("$root_dst","ROOT")) | Where { $_.SideIndicator -eq '<=' } | measure | % {$_.count }
+
+# Compare-Object
+$text_src = "C:\temp\src.txt"
+$text_dst = "C:\temp\dst.txt"
+Compare-Object -ReferenceObject $(Get-Content $text_src) -DifferenceObject $(Get-Content $text_dst)
+
+
+# Compare array of files, based on fullname, but replace parts of fullname with ROOT (e.g. beginning)
+# Filters: Find ReparsePoint in SRC, and only files (ignore olders)
+# NOTE: Older PowerShell may not understand "-file". May use where {! $_.PSIsContainer }
+$root_src = "C:\temp\powershell\src"
+$root_dst = "C:\temp\powershell\dst"
+$files_src = gci -file $root_src -recurse | where {$_.attributes -match 'ReparsePoint'} | select -ExpandProperty fullname
+$files_dst = gci -file $root_dst -recurse | select -ExpandProperty fullname
+# Measure results, and display count
+$files_src | measure | % {$_.count }
+$files_dst | measure | % {$_.count }
+# Compare results, using Compare-Object
+$compare_src = ($files_src).replace("$root_src","ROOT")
+$compare_dst = ($files_dst).replace("$root_dst","ROOT")
+Compare-Object -ReferenceObject $compare_src -DifferenceObject $compare_dst | Where { $_.SideIndicator -eq '<=' }
+# Compare-Object. Measure left only (count)
+Compare-Object -ReferenceObject $compare_src -DifferenceObject $compare_dst | Where { $_.SideIndicator -eq '<=' } | measure | % {$_.count }
