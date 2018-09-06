edb7e0006fd81e202026051aeb2038123f09f4e1
Espen Ødegaard <esodesod@users.noreply.github.com>
2018-09-06 15:46:13 +0200

    Cleaned up a little

--- powershell/windows/compare-results-in-two-arrays-or-list-primary.ps1
+++ powershell/windows/compare-results-in-two-arrays-or-list-primary.ps1
@@ -5,31 +5,51 @@
 # * Only files (ignore folders)
 # NOTE: Older PowerShell may not understand "-file". May use where {! $_.PSIsContainer }
 
-# Set variables
+# Set source and destination for compare
 $root_src = "C:\temp\powershell\src"
 $root_dst = "C:\temp\powershell\dst"
-# Get files from both directories. Only ReparsePoints files from source
+# Get all files from both directories
+# Filter: Only ReparsePoints files from source
 $files_src = Get-ChildItem -file $root_src -recurse | Where-Object {$_.attributes -match 'ReparsePoint'} | Select-Object -ExpandProperty fullname
 $files_dst = Get-ChildItem -file $root_dst -recurse | Select-Object -ExpandProperty fullname
-# Do a simple "measure" to display count of files
+# Measure (count) objects in both locations
+Write-Host "Count of files found in $root_src"
 $files_src | Measure-Object | ForEach-Object {$_.count }
+Write-Host "Count of files found in $root_dst"
 $files_dst | Measure-Object | ForEach-Object {$_.count }
 # Set compare variables, using previous variables, and replace parts of fullpath
 $compare_src = ($files_src).replace("$root_src","ROOT")
 $compare_dst = ($files_dst).replace("$root_dst","ROOT")
+
+# -------------------------
+# --- COMPARE LEFT ONLY ---
+# -------------------------
 # Do a compare. Get "left only" objects
 $compare_left_only_objects = Compare-Object -ReferenceObject $compare_src -DifferenceObject $compare_dst | Where-Object { $_.SideIndicator -eq '<=' }
 # Count results
+Write-Host "Count of files: LEFT ONLY / MISSING IN $root_dst"
 $compare_left_only_objects | Measure-Object | ForEach-Object {$_.count }
 # Show results
+Write-Host "Files: LEFT ONLY / MISSING IN $root_dst"
 $compare_left_only_objects
 
-# Do a new compare, and time (measure) command
-Measure-Command { Compare-Object -ReferenceObject $compare_src -DifferenceObject $compare_dst | Where-Object { $_.SideIndicator -eq '<=' } }
-
+# -------------------------
+# --- COMPARE IDENTICAL ---
+# -------------------------
 # Do a compare. Get "identical" objects
 $compare_identical_objects = Compare-Object -ReferenceObject $compare_src -DifferenceObject $compare_dst -IncludeEqual | Where-Object { $_.SideIndicator -eq '==' }
 # Count results
+Write-Host "Count of files: IDENTICAL / found in both directories"
 $compare_identical_objects | Measure-Object | ForEach-Object {$_.count }
 # Show results
+Write-Host "Files: IDENTICAL / found in both directories"
 $compare_identical_objects
+
+# -------------------------
+# --- TIMINGS ---
+# -------------------------
+# Do a new compare, and time (measure) command
+Write-Host "Timing compare of LEFT ONLY objects"
+Measure-Command { Compare-Object -ReferenceObject $compare_src -DifferenceObject $compare_dst | Where-Object { $_.SideIndicator -eq '<=' } }
+Write-Host "Timing compare of IDENTICAL objects"
+Measure-Command { Compare-Object -ReferenceObject $compare_src -DifferenceObject $compare_dst -IncludeEqual | Where-Object { $_.SideIndicator -eq '==' } }
