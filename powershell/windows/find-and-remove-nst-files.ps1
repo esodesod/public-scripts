22d1e86018e1e143e863060c890e0d50ed7d9438
Espen Ødegaard <esodesod@users.noreply.github.com>
2018-10-15 12:38:13 +0200

    Update powershell/windows/find-and-remove-nst-files.ps1

--- powershell/windows/find-and-remove-nst-files.ps1
+++ powershell/windows/find-and-remove-nst-files.ps1
@@ -1,5 +1,5 @@
 # Quick information
-# * Find files, based on criteria, e.g. *.nst-files
+# * Find files, based on criteria (in this case; *.nst-files, length (size) over "16818176" and lastewrite)
 # * If found, based on criteria, remove files older then x-days (e.g. based on lastewrite / modified date)
 # ####################################################################################################################
 
@@ -26,17 +26,27 @@ $extension = "*.nst"
 
 # ####################################################################################################################
 # get files based on lastwrite
-$files = Get-ChildItem $targetfolder -include $extension -Recurse | where {$_.LastwriteTime -lt "$lastwrite"}
-# measure the total size of files that will be deleted
+$files = Get-ChildItem $targetfolder -include $extension -Recurse | Where-Object {$_.LastwriteTime -lt "$lastwrite" -and $_.Length -gt "16818176"}
+# show files (run manually or uncomment section)
+<#
+# show files, sort by "default"
+Write-Output $files | Select-Object fullname,@{Name='SizeMB';Expression={$_.Length / 1MB}} | Format-Table -AutoSize
+
+# show files, sort by length (size)
+Write-Output $files | Sort-Object length -desc | Select-Object fullname,@{Name='SizeMB';Expression={$_.Length / 1MB}} | Format-Table -AutoSize
+
+# measure the total length (size) of all files that will be deleted
 $totalSize = ($files | Measure-Object -Sum Length).Sum / 1GB
 Write-Host Total size on all files is: $totalSize -ForegroundColor Green
+#>
+
 # delete files
-foreach ($File in $Files)
+foreach ($file in $files)
 {
-    if ($File -ne $Null)
+    if ($file -ne $null)
     {
-        Write-Host "Deleting file $File" -backgroundcolor "DarkRed"
-        Remove-item $File.Fullname | out-null
+        Write-Host "Deleting file $file" -backgroundcolor "DarkRed"
+        Remove-item $file.Fullname | out-null
     }
     else
     {
