8a272ad32bb415a6301ae40d2c8e5d184472be0e
Espen Ødegaard <Espen.Odegaard@odfjell.com>
2019-01-07 15:42:11 +0100

    Updated to use LiteralPath
    Due to [1] / files with special characters

--- powershell/windows/reparsepoint-p02-create-textfile-with-information-and-delete-source.ps1
+++ powershell/windows/reparsepoint-p02-create-textfile-with-information-and-delete-source.ps1
@@ -20,18 +20,18 @@ foreach ($file in $files_dst) {
         # Outfile: Set header
         $header = "This file was archived to vault (due to previous archiving policies), and was not able to automatically restore in migration process. We created this placeholder file, just in case. If the file is needed, please contact helpdesk, and we'll try to locate the file in older backups, if possible. Please bear in mind that this is a really time consuming process (hours/days), and if other (faster) re-creation means is possible, please try them first."
         # Outfile: Append header to outfile
-        $header | Out-File $outfile -Append -WhatIf
-        Write-Output "TECHNICAL INFORMATION - About the ReparsePoint file" | Out-File $outfile -Append -WhatIf
+        $header | Out-File -LiteralPath $outfile -Append
+        Write-Output "TECHNICAL INFORMATION - About the ReparsePoint file" | Out-File -LiteralPath $outfile -Append
         # Outfile: Append information to outfile
-        $file | Format-List * | Out-File $outfile -Append -WhatIf
+        $file | Format-List * | Out-File -LiteralPath $outfile -Append
         # Check if outfile exists, and if so, remove the ReparsePoint
-        $outfile_check = (Test-Path $outfile)
+        $outfile_check = (Test-Path -LiteralPath $outfile)
         if ($outfile_check -eq $True) {
             # Debug
             Write-Host $outfile "successfully located, you may remove the ReparsePoint now" -ForegroundColor Green
             # Remove the ReparsePoint
             Write-Host $outfile "removing the ReparsePoint file" -ForegroundColor Green
-            Remove-Item $file.FullName -WhatIf
+            Remove-Item -LiteralPath $file.FullName
         }
         if ($outfile_check -eq $False) {
             # Debug
