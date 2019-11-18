a3ede99c58437619035af55e5831eb83a8d18e09
Espen Ødegaard <Espen.Odegaard@odfjell.com>
2019-11-18 10:55:37 +0100

    Update Microsoft.PowerShell_profile.ps1

--- powershell/profile/Microsoft.PowerShell_profile.ps1
+++ powershell/profile/Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,19 @@
 # Remove Hyper-V modules, cause in conflict with VMware admins (modules)
 (Get-Module -Name Hyper-V) | Remove-Module -Force -ErrorAction SilentlyContinue
 
-$psdir="H:\github\esodesod\scripts\powershell\functions"
-Get-ChildItem "$psdir\*.ps1" | ForEach-Object {.$_}
-Write-Host "Custom esod.no PS-environment loaded" -ForegroundColor DarkGreen
+# Set policy
+Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass
+
+#<# Test paths for *.ps1 & load (if found)
+$test_powershell_fuctions_path1 = "$($env:USERPROFILE)\GitHub\esodesod\scripts\powershell\functions"
+if (Test-Path $test_powershell_fuctions_path1) {
+    $result_test_powershell_fuctions_path1 = $test_powershell_fuctions_path1
+    Write-Host "INFO: Found PowerShell functions in $result_test_powershell_fuctions_path1" -ForegroundColor DarkGray
+    Get-ChildItem "$($result_test_powershell_fuctions_path1)\*.ps1" | ForEach-Object {
+        Write-Host "INFO: Loading $_" -ForegroundColor DarkGray
+        .$_
+    }
+    Write-Host "Custom esod.no PowerShell environment loaded" -ForegroundColor DarkGreen
+}
+
+#>
