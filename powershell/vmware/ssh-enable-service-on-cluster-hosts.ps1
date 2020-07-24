a8da0aac14d36728f8740f37325f5af837c65598
esodesod <esodesod@users.noreply.github.com>
2020-07-24 10:29:41 +0200

    Create ssh-enable-service-on-cluster-hosts.ps1

---
+++ powershell/vmware/ssh-enable-service-on-cluster-hosts.ps1
@@ -0,0 +1,8 @@
+﻿# Set cluster name
+$cluster = "cluster-01-name"
+
+# Get hosts in cluster, and enable TSH-SSH on all hosts
+Get-Cluster -name $cluster | Get-VMhost | Foreach {
+    Write-Host "Enabling SSH on server $($_.Name)"
+    Get-VMHostService -VMHost $_ | Where { $_.Key -eq "TSM-SSH" } | Start-VMHostService
+}
